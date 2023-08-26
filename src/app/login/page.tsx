f09b0e3489cfd774a94e91722086b18f392fe93e
Chris <rolletchris1@gmail.com>
2023-08-26 08:37:15 -0400

    update tailwind design

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -68,39 +68,42 @@ export default function Page() {
 		<div className={`body ${isDarkMode ? styles.body : 'light-mode'}`}>
 			<div className="flex flex-col items-center justify-center min-h-screen w-full">
 				<div className="flex justify-between items-center w-full p-20 absolute top-0 left-0 max-w-full box-border">
-					<h1>Cursif</h1>
+					<h1 className="font-montez font-normal text-6xl leading-8">Cursif</h1>
 
 					<div>
 						<button onClick={toggleDarkMode}>{isDarkMode ? 'Light Mode' : 'Dark Mode'}</button>
 					</div>
 
-					<div className="px-3 py-1 rounded-md">
-						<button className={`border-none text-sm px-3 py-2 transition duration-300 
+					<div className="relative w-182 h-33 top-51 left-1211">
+						<button className={`w-full h-full font-roboto font-normal text-2xl leading-7 text-center px-3 py-2 rounded-md transition duration-300
 						${isDarkMode ? 'bg-white text-black' : 'bg-black text-white'
 							}`}>Register</button>
 					</div>
 				</div>
 
-				<div className="p-60 w-800 text-center flex flex-col items-center">
-					<h2><b>LOG</b> IN</h2>
+				<div className="relative">
+					<h2 className="mb-24 text-5xl text-center">
+						<b className="font-semibold">LOG</b> IN
+					</h2>
 					<div>
 						<input
-							className="bg-opacity-90 p-4 mb-15 border border-gray-300 rounded-md my-5 transition duration-300 bg-gray-300 hover:bg-opacity-100 hover:text-black"
+							className={`p-3 px-14 mb-15 border border-gray-800 rounded-md my-3 transition duration-300 ${isDarkMode ? 'bg-gray-700 text-white' : 'bg-white text-black hover:bg-opacity-100 hover:text-black'}`}
 							type="text"
 							placeholder="Email"
 							value={email}
 							onChange={handleEmailChange}
 						/>
+
 						<div className="relative">
 							<input
-								className="bg-opacity-90 p-4 mb-5 border border-gray-300 rounded-md my-5 transition duration-300 bg-gray-300 hover:bg-opacity-100 hover:text-black"
+								className={`p-3 px-14 mb-15 border border-gray-800 rounded-md my-3 transition duration-300 ${isDarkMode ? 'bg-gray-700 text-white' : 'bg-white text-black hover:bg-opacity-100 hover:text-black'}`}
 								type={showPassword ? 'text' : 'password'}
 								placeholder="Password"
 								value={password}
 								onChange={handlePasswordChange}
 							/>
 							<button
-								className="absolute top-12 right-1 transform -translate-y-1/2 border-none bg-transparent cursor-pointer"
+								className="absolute top-10 right-1 transform -translate-y-1/2 border-none bg-transparent cursor-pointer"
 								onClick={handlePasswordToggle}
 							>
 								{showPassword ? <FaEyeSlash /> : <FaEye />}
