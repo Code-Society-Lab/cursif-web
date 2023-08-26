a66a14bacfaf5ba0145d0a531fa5bb9786e95696
Chris <rolletchris1@gmail.com>
2023-08-25 21:02:34 -0400

    refactor + added tailwind

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,141 +1,121 @@
 "use client"
 
-import styles from './styles.module.css';
-
 import React, { useState } from 'react';
 import { FaEye, FaEyeSlash } from 'react-icons/fa';
 import { useMutation, gql } from '@apollo/client';
 import { useDarkMode } from '@/app/dark-mode-context';
-
-import { Button } from '@/app/components/button';
+import styles from './styles.module.css'
 
 const LOGIN_MUTATION = gql`
-    mutation Login($email: String!, $password: String!) {
-        login(email: $email, password: $password) {
-            token
-            user {
-                email
-                firstName
-                id
-                lastName
-                username
-            }
-        }
-    }
+		mutation Login($email: String!, $password: String!) {
+				login(email: $email, password: $password) {
+						token
+						user {
+								email
+								firstName
+								id
+								lastName
+								username
+						}
+				}
+		}
 `;
 
 export default function Page() {
-  const { isDarkMode, toggleDarkMode } = useDarkMode();
+	const { isDarkMode, toggleDarkMode } = useDarkMode();
 
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+	const [loginMutation] = useMutation(LOGIN_MUTATION);
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const [showPassword, setShowPassword] = useState(false);
 
-  const handleEmailChange = (event) => {
-    setEmail(event.target.value);
-  };
+	const handlePasswordToggle = () => {
+		setShowPassword(!showPassword);
+	};
 
-  const handlePasswordChange = (event) => {
-    setPassword(event.target.value);
-  };
+	const handleEmailChange = (event) => {
+		setEmail(event.target.value);
+	};
 
-  const containerStyle = {
-    backgroundColor: isDarkMode ? '#404040' : 'white',
-    color: isDarkMode ? 'white' : 'black',
-  };
+	const handlePasswordChange = (event) => {
+		setPassword(event.target.value);
+	};
 
-  const buttonStyle = {
-    backgroundColor: isDarkMode ? 'white' : '#404040',
-    color: isDarkMode ? 'black' : 'white',
-  };
+	const handleLogin = async () => {
+		try {
+			const { data } = await loginMutation({
+				variables: {
+					email: email,
+					password: password,
+				},
+			});
 
-  const inputStyle = {
-    backgroundColor: isDarkMode ? '#404040' : '#dfdfdfdd',
-    color: isDarkMode ? 'black' : 'white',
-  };
+			if (data && data.login && data.login.user) {
+				const username = data.login.user.username;
+				console.log('Login successful. Username:', username);
 
-  const [loginMutation] = useMutation(LOGIN_MUTATION);
+				// Redirect to home screen
+				window.location.href = '/';
+			} else {
+				console.log('Login failed. Please check your credentials.');
+			}
+		} catch (error) {
+			console.error('Error logging in:', error);
+		}
+	};
 
-  const handleLogin = async () => {
-    try {
-      const { data } = await loginMutation({
-        variables: {
-          email: email,
-          password: password,
-        },
-      });
+	return (
+		<div className={`body ${isDarkMode ? styles.body : 'light-mode'}`}>
+			<div className="flex flex-col items-center justify-center min-h-screen w-full">
+				<div className="flex justify-between items-center w-full p-20 absolute top-0 left-0 max-w-full box-border">
+					<h1>Cursif</h1>
 
-      if (data && data.login && data.login.user) {
-        const username = data.login.user.username;
-        console.log('Login successful. User ID:', username);
-        
-        // Redirect to home screen
-        window.location.href = '/';
-      } else {
-        console.log('Login failed. Please check your credentials.');
-      }
-    } catch (error) {
-      console.error('Error logging in:', error);
-    }
-  };
+					<div>
+						<button onClick={toggleDarkMode}>{isDarkMode ? 'Light Mode' : 'Dark Mode'}</button>
+					</div>
 
-  const [showPassword, setShowPassword] = useState(false);
+					<div className="px-3 py-1 rounded-md">
+						<button className={`border-none text-sm px-3 py-2 transition duration-300 
+						${isDarkMode ? 'bg-white text-black' : 'bg-black text-white'
+							}`}>Register</button>
+					</div>
+				</div>
 
-  const handlePasswordToggle = () => {
-    setShowPassword(!showPassword);
-  };
+				<div className="p-60 w-800 text-center flex flex-col items-center">
+					<h2><b>LOG</b> IN</h2>
+					<div>
+						<input
+							className="bg-opacity-90 p-4 mb-15 border border-gray-300 rounded-md my-5 transition duration-300 bg-gray-300 hover:bg-opacity-100 hover:text-black"
+							type="text"
+							placeholder="Email"
+							value={email}
+							onChange={handleEmailChange}
+						/>
+						<div className="relative">
+							<input
+								className="bg-opacity-90 p-4 mb-5 border border-gray-300 rounded-md my-5 transition duration-300 bg-gray-300 hover:bg-opacity-100 hover:text-black"
+								type={showPassword ? 'text' : 'password'}
+								placeholder="Password"
+								value={password}
+								onChange={handlePasswordChange}
+							/>
+							<button
+								className="absolute top-12 right-1 transform -translate-y-1/2 border-none bg-transparent cursor-pointer"
+								onClick={handlePasswordToggle}
+							>
+								{showPassword ? <FaEyeSlash /> : <FaEye />}
+							</button>
+						</div>
 
-  return (
-    <div className={styles.container} style={containerStyle}>
-      <div className={styles.container}>
-        <div className={styles.header}>
-          <h1>Cursif</h1>
+						<div className="float-left text-sm">
+							<a href="#">Forgot Password?</a>
+						</div>
 
-          <div className={`${styles.cornerText} ${isDarkMode ? styles.whiteCornerText : styles.blackCornerText}`}>
-            <button onClick={toggleDarkMode}>{isDarkMode ? 'Light Mode' : 'Dark Mode'}</button>
-          </div>
-
-          <div className={styles.registerButtonContainer}>
-            <button className={styles.registerButton} style={buttonStyle}>Register</button>
-          </div>
-        </div>
-
-        <div className={styles.loginBox}>
-          <h2><span className={styles.boldText}>LOG</span> IN</h2>
-          <div className={styles.inputContainer}>
-            <input
-              className={styles.input}
-              style={inputStyle}
-              type="text"
-              placeholder="Email"
-              value={email}
-              onChange={handleEmailChange}
-            />
-            <div className={styles.passwordInput}>
-              <input
-                className={styles.input}
-                style={inputStyle}
-                type={showPassword ? 'text' : 'password'}
-                placeholder="Password"
-                value={password}
-                onChange={handlePasswordChange}
-              />
-              <button
-                className={styles.showPasswordButton}
-                onClick={handlePasswordToggle}
-              >
-                {showPassword ? <FaEyeSlash /> : <FaEye />}
-              </button>
-            </div>
-
-            <div className={styles.forgotPassword}>
-              <a href="#">Forgot Password?</a>
-            </div>
-
-            {/* Using float isn't the best approach but until this is full refactored it's fine*/}
-            <button className="button accent mt-8 float-right" onClick={handleLogin}>Login</button>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+						{/* Using float isn't the best approach but until this is full refactored it's fine*/}
+						<button className="button accent mt-8 float-right" onClick={handleLogin}>Login</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
