be19f77b3e74b054ed288c37218cd4d8a000a2cb
Chris <rolletchris1@gmail.com>
2023-08-26 11:51:19 -0400

    update dark mode

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,6 @@
 "use client"
 
-import styles from './styles.module.css'
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { FaEye, FaEyeSlash } from 'react-icons/fa';
 import { useMutation, gql } from '@apollo/client';
 import { useDarkMode } from '@/app/dark-mode-context';
@@ -23,6 +22,15 @@ const LOGIN_MUTATION = gql`
 
 export default function Page() {
 	const { isDarkMode, toggleDarkMode } = useDarkMode();
+	const [systemPreferenceDarkMode, setSystemPreferenceDarkMode] = useState(false);
+
+  useEffect(() => {
+    // Check the user's system preference for dark mode
+    const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    setSystemPreferenceDarkMode(prefersDarkMode);
+  }, []);
+
+  const initialDarkMode = systemPreferenceDarkMode || isDarkMode;
 
 	const [loginMutation] = useMutation(LOGIN_MUTATION);
 	const [email, setEmail] = useState('');
@@ -65,18 +73,18 @@ export default function Page() {
 	};
 
 	return (
-		<div className={`body ${isDarkMode ? styles.body : 'light-mode'}`}>
+		<div className={`body ${initialDarkMode ? 'dark-mode' : 'light-mode'}`}>
 			<div className="flex flex-col items-center justify-center min-h-screen w-full">
-				<div className="flex justify-between items-center w-full p-20 absolute top-0 left-0 max-w-full box-border">
-					<h1 className="font-montez font-normal text-6xl leading-8">Cursif</h1>
+				<div className="flex justify-between items-center w-full p-8 absolute top-0 left-0 max-w-full box-border">
+					<h1 className="font-montez font-normal text-5xl leading-8">Cursif</h1>
 
 					<div>
-						<button onClick={toggleDarkMode}>{isDarkMode ? 'Light Mode' : 'Dark Mode'}</button>
+						<button onClick={toggleDarkMode}>{initialDarkMode ? 'Light Mode' : 'Dark Mode'}</button>
 					</div>
 
 					<div className="relative w-182 h-33 top-51 left-1211">
 						<button className={`w-full h-full font-roboto font-normal text-2xl leading-7 text-center px-3 py-2 rounded-md transition duration-300
-						${isDarkMode ? 'bg-white text-black' : 'bg-black text-white'
+						buttonRegister ${initialDarkMode ? 'bg-color-light' : 'bg-color-dark'
 							}`}>Register</button>
 					</div>
 				</div>
@@ -87,7 +95,7 @@ export default function Page() {
 					</h2>
 					<div>
 						<input
-							className={`p-3 px-14 mb-15 border border-gray-800 rounded-md my-3 transition duration-300 ${isDarkMode ? 'bg-gray-700 text-white' : 'bg-white text-black hover:bg-opacity-100 hover:text-black'}`}
+							className={`p-3 px-14 mb-15 rounded-md my-3 transition duration-300 ${initialDarkMode ? 'bg-neutral-900 text-white' : 'bg-gray-200 text-black hover:bg-opacity-100 hover:text-black'}`}
 							type="text"
 							placeholder="Email"
 							value={email}
@@ -96,14 +104,14 @@ export default function Page() {
 
 						<div className="relative">
 							<input
-								className={`p-3 px-14 mb-15 border border-gray-800 rounded-md my-3 transition duration-300 ${isDarkMode ? 'bg-gray-700 text-white' : 'bg-white text-black hover:bg-opacity-100 hover:text-black'}`}
+								className={`p-3 px-14 mb-15 rounded-md my-3 transition duration-300 ${initialDarkMode ? 'bg-neutral-900 text-white' : 'bg-gray-200 text-black hover:bg-opacity-100 hover:text-black'}`}
 								type={showPassword ? 'text' : 'password'}
 								placeholder="Password"
 								value={password}
 								onChange={handlePasswordChange}
 							/>
 							<button
-								className="absolute top-10 right-1 transform -translate-y-1/2 border-none bg-transparent cursor-pointer"
+								className="absolute top-10 right-3 transform -translate-y-1/2 border-none bg-transparent cursor-pointer"
 								onClick={handlePasswordToggle}
 							>
 								{showPassword ? <FaEyeSlash /> : <FaEye />}
