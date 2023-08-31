05ea3ad28a3e7fc0e8988bc272dad34ed07071dc
Simon Roy <simon.roy1211@gmail.com>
2023-08-31 04:04:11 -0400

    Fixed ENV VAR not working

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,6 @@
 const Config = {
   graphql: {
-    endpoint: process.env.REACT_GRAPHQL_ENDPOINT || 'http://localhost:4000/api'
+    endpoint: process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT || 'http://localhost:4000/api'
   }
 }
 
