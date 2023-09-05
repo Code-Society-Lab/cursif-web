ad2c18d9e9b0316f30cb27a349210f5e22d48fc1
Simon Roy <simon.roy1211@gmail.com>
2023-09-05 03:13:08 -0400

    Added some documentation in AuthProvider

--- src/components/auth-provider.tsx
+++ src/components/auth-provider.tsx
@@ -18,7 +18,56 @@ const GET_ME = gql`
   }
 `;
 
-export function AuthProvider({ children }) {
+/*
+ * The AuthProvider component serves as a higher-level context provider for managing user
+ * authentication in your application. It uses Apollo Client to make a GraphQL  query to fetch
+ * current user data when the component is mounted and provides the authenticated user
+ * information to the rest of your application via the AuthContext.
+ *
+ * # Usage
+ * To use the AuthProvider, you should wrap your application's root component (layout.tsx)
+ * with it. This ensures that the authentication context is available throughout
+ * your application. To access the user called `useUser`.
+ *
+ * # Example
+  * // web/src/app/layout.tsx
+  * ```
+  * import { AuthProvider, useAuth } from '@redwoodjs/auth'
+  *
+  * export default function RootLayout({ children }: { children: React.ReactNode }) {
+  *   return (
+  *     <ApolloWrapper>
+  *       <html lang="en">
+  *         <body className="min-w-[350px]">
+  *           {children}
+  *         </body>
+  *       </html>
+  *     </ApolloWrapper>
+  *   );
+  * }
+  * ```
+  *
+  * // web/src/pages/index.tsx
+  * ```
+  * import { useAuth } from '@redwoodjs/auth'
+  *
+  *  "use client"
+  *
+  *  import React from 'react';
+  *  import { useAuth } from '@components/auth-provider';
+  *
+  *  export default function Page(): JSX.Element {
+  *    const { user } = useAuth();
+  *
+  *    return (
+  *      <div>
+  *        <div>USERNAME: {user?.username}</div>
+  *      </div>
+  *    );
+  *  }
+  * ```
+ */
+export function AuthProvider({ children }: { children: React.ReactNode }) {
   const router = useRouter();
   const [user, setUser] = useState(null);
   const { data, loading, error } = useQuery(GET_ME, {
