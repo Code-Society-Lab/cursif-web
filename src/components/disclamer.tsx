e951e8f615f15cc33353c78b3d7207a5eca6f393
Chris <rolletchris1@gmail.com>
2023-10-27 11:49:45 -0700

    Update Fun name + edited text

--- src/components/disclamer.tsx
+++ src/components/disclamer.tsx
@@ -1,11 +1,10 @@
-export default function Disclamer() {
+export default function DisclamerBanner() {
   return (
-      <div className="fixed top-0 w-full bg-yellow-400 p-0 text-center">
-        <p className="text-sm text-gray-800">
-          <b>Disclaimer</b>: We are still in the development phase.
-          Data loss could occur.
-          Please use at your own risk. <a href="/disclaimer" className="underline text-blue-600">Read our disclaimer</a>
-        </p>
-      </div>
+    <div className="fixed top-0 w-full bg-yellow-400 p-0 text-center">
+      <p className="text-sm text-gray-800">
+        <a href="/disclaimer" className="underline text-blue-600"><b>Disclaimer</b></a>:
+        We are still in the development phase. Data loss could occur. Please use at your own risk.
+      </p>
+    </div>
   );
 }
