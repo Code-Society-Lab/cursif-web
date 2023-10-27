c02b298f07ae94ed03d67a4d569cb9560f56276f
Chris <rolletchris1@gmail.com>
2023-10-27 14:05:32 -0700

    update layout to add the WarningBanner Wrapper

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,9 @@ import '@styles/themes/dark.css'
 import '@styles/themes/light.css'
 
 import { ApolloWrapper } from "@/components/graphql/apollo-wrapper";
-import DisclaimerBanner from '@/components/disclaimer';
+import WarningBanner from '@/components/disclaimer';
 import type { Metadata } from 'next'
+import Config from '../config';
 
 export const metadata: Metadata = {
   title: 'Cursif',
@@ -16,7 +17,17 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <ApolloWrapper>
       <html lang="en">
         <body className="min-w-[350px]">
-          <DisclaimerBanner />
+          <WarningBanner if={Config.production()}>
+            <>
+              Cursif is currently under heavy <b>development</b>.
+              Data loss could occur.
+              Please use at your own risk. <a href="https://github.com/Code-Society-Lab/cursif" className="underline text-blue-600"><b>Read more</b></a>
+            </>
+          </WarningBanner>
+
+          <WarningBanner if={Config.development()}>
+            You are currently in <b>development!</b>
+          </WarningBanner>
           {children}
         </body>
       </html>
