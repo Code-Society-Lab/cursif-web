d450c9b3859b33017ebe498693b3c34112c2b0d9
Chris <rolletchris1@gmail.com>
2023-10-27 11:43:30 -0700

    Create a disclamer banner component

---
+++ src/components/disclamer.tsx
@@ -0,0 +1,11 @@
+export default function Disclamer() {
+  return (
+      <div className="fixed top-0 w-full bg-yellow-400 p-0 text-center">
+        <p className="text-sm text-gray-800">
+          <b>Disclaimer</b>: We are still in the development phase.
+          Data loss could occur.
+          Please use at your own risk. <a href="/disclaimer" className="underline text-blue-600">Read our disclaimer</a>
+        </p>
+      </div>
+  );
+}
