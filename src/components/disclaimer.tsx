ed33aa66b45aea5bac537cae38b21aa8998d1d57
Chris <rolletchris1@gmail.com>
2023-10-27 12:22:14 -0700

    update flex banner + message

--- src/components/disclaimer.tsx
+++ src/components/disclaimer.tsx
@@ -1,9 +1,10 @@
 export default function DisclaimerBanner() {
   return (
-    <div className="fixed top-0 w-full bg-yellow-400 p-0 text-center">
-      <p className="text-sm text-gray-800">
-        <a href="/disclaimer" className="underline text-blue-600"><b>Disclaimer</b></a>:
-        We are still in the development phase. Data loss could occur. Please use at your own risk.
+    <div className="flex justify-center w-full bg-yellow-400 p-4">
+      <p className="text-sm text-gray-800 text-center">
+        <b>Disclaimer</b>: We are still in the development phase.
+        Data loss could occur.
+        Please use at your own risk. <a href="https://github.com/Code-Society-Lab/cursif" className="underline text-blue-600"><b>Read more</b></a>
       </p>
     </div>
   );
