8abf2d7fb34fe8387c2ebf0d377885d35d27d08e
Chris <rolletchris1@gmail.com>
2023-10-27 13:34:20 -0700

    fix repetitive code

--- src/components/disclaimer.tsx
+++ src/components/disclaimer.tsx
@@ -1,23 +1,23 @@
 import Config from "../config";
 
 export default function DisclaimerBanner() {
-  if (Config.production()) {
-    return (
-      <div className="flex justify-center w-full bg-yellow-400 p-4">
-        <p className="text-sm text-gray-800 text-center">
-          Cursif is currently under heavy <b>developement</b>. 
-          Data loss could occur. 
-          Please use at your own risk. <a href="https://github.com/Code-Society-Lab/cursif" className="underline text-blue-600"><b>Read more</b></a>
-        </p>
-      </div>
-    );
-  }
-  else {
-    return (
-      <div className="flex justify-center w-full bg-yellow-400 p-2">
-        <p className="text-sm text-gray-800 text-center">
-          You are currently in <b>developement!</b></p>
-      </div>
-    );
-  }
+  return (
+    <>
+      {
+        Config.production() || Config.development() ? (
+          <div className="flex justify-center w-full bg-yellow-400 p-4">
+            <p className="text-sm text-gray-800 text-center">
+              {
+                Config.production() ? (
+                  <>
+                    Cursif is currently under heavy <b>development</b>.
+                    Data loss could occur.
+                    Please use at your own risk. <a href="https://github.com/Code-Society-Lab/cursif" className="underline text-blue-600"><b>Read more</b></a>
+                  </>) : <>You are currently in <b>developement!</b></>
+              }
+            </p>
+          </div>) : null
+      }
+    </>
+  );
 }
