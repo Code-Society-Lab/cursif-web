8964e78cbff9ddef1d1de98555dc0a1152323366
Chris <rolletchris1@gmail.com>
2023-10-27 13:00:09 -0700

    Updated banner for dev and prod env

--- src/components/disclaimer.tsx
+++ src/components/disclaimer.tsx
@@ -1,11 +1,23 @@
+import Config from "../config";
+
 export default function DisclaimerBanner() {
-  return (
-    <div className="flex justify-center w-full bg-yellow-400 p-4">
-      <p className="text-sm text-gray-800 text-center">
-        <b>Disclaimer</b>: We are still in the development phase.
-        Data loss could occur.
-        Please use at your own risk. <a href="https://github.com/Code-Society-Lab/cursif" className="underline text-blue-600"><b>Read more</b></a>
-      </p>
-    </div>
-  );
+  if (Config.production()) {
+    return (
+      <div className="flex justify-center w-full bg-yellow-400 p-4">
+        <p className="text-sm text-gray-800 text-center">
+          Cursif is currently under heavy <b>developement</b>. 
+          Data loss could occur. 
+          Please use at your own risk. <a href="https://github.com/Code-Society-Lab/cursif" className="underline text-blue-600"><b>Read more</b></a>
+        </p>
+      </div>
+    );
+  }
+  else {
+    return (
+      <div className="flex justify-center w-full bg-yellow-400 p-2">
+        <p className="text-sm text-gray-800 text-center">
+          You are currently in <b>developement!</b></p>
+      </div>
+    );
+  }
 }
