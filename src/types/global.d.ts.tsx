a279d5d961a4ce357c501b9eea1fa7316ee0aa97
Chris <rolletchris1@gmail.com>
2023-11-07 18:30:47 -0800

    Added our own data types for Notebook and Page

---
+++ src/types/global.d.ts.tsx
@@ -0,0 +1,28 @@
+// Desc: Global types for the Cursif
+export { };
+
+declare global {
+  interface Page {
+    id: string
+    title: string
+    content: string
+    parent_id: number
+    parent_type: string
+    parent: Parent
+    children: Page[]
+  }
+
+  interface Notebook {
+    id: string
+    title: string
+    description: string
+    owner_id: number
+    owner_type: string
+    pages: Page[]
+  }
+
+  interface Parent {
+    pages: Page[]
+    notebooks: Notebook[]
+  }
+}
