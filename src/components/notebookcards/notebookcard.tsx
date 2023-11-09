7b4ce7a1c9309c4bd64c1d9925fe7f87d07fbcfc
piro56 <69094266+piro56@users.noreply.github.com>
2023-11-09 18:05:44 -0500

    remove dynamic tooltip

--- src/components/notebookcards/notebookcard.tsx
+++ src/components/notebookcards/notebookcard.tsx
@@ -6,31 +6,9 @@ export interface NotebookCardInterface {
     lastupdated?: string;
 }
 export default function NotebookCard(props: NotebookCardInterface) {
-    const [titleTooltip, setTitleTooltip] = useState<string>('');
-    const [descTooltip, setDescTooltip] = useState<string>('');
 
-    const h1Ref = useRef<HTMLHeadingElement | null>(null);
-    const descRef = useRef<HTMLParagraphElement | null>(null);
 
     const lastUpdated = "Last Updated : " + (props.lastupdated ? props.lastupdated : 'Unknown');
-
-    useEffect(() => {
-        if (h1Ref.current) {
-            if (h1Ref.current.offsetWidth < h1Ref.current.scrollWidth) {
-                setTitleTooltip(props.title);
-            } else {
-                setTitleTooltip('');
-            }
-        }
-    }, [props.title]);
-    useEffect(() => {
-        if (descRef.current && (descRef.current.offsetWidth < descRef.current.scrollWidth ||
-            descRef.current.offsetHeight < descRef.current.scrollHeight)) {
-            setDescTooltip(props.description);
-        } else {
-            setDescTooltip('');
-        }
-    }, [props.description]);
 
 
 
@@ -41,12 +19,12 @@ export default function NotebookCard(props: NotebookCardInterface) {
             <div className="flex flex-col pl-2 pt-2 min-h-[100px] max-h-[150px]">
 
                 <span>
-                    <h1 title={titleTooltip} ref={h1Ref}
+                    <h1 title={props.title}
                         className="text-lg font-bold tracking-tight whitespace-nowrap text-gray-900 text-white 
                 overflow-hidden text-ellipsis w-min max-w-[95%]">{props.title}</h1>
                 </span>
                 <span className="basis-16">
-                    <p title={descTooltip} ref={descRef}
+                    <p title={props.description} 
                         className="font-normal text-sm text-gray-400 overflow-hidden break-all paragraph-wrap-3 max-w-[95%]">{props.description}</p>
                 </span>
                 <p className="basis-2"></p>
