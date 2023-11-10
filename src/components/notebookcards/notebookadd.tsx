85fd90fa84a93b3a21c583cfec2d8792c73165f9
piro56 <69094266+piro56@users.noreply.github.com>
2023-11-09 20:07:43 -0500

    Update notebookadd.tsx

--- src/components/notebookcards/notebookadd.tsx
+++ src/components/notebookcards/notebookadd.tsx
@@ -1,6 +1,5 @@
 
 export default function NotebookAddCard() {
-
     return (
         <a href="#" className="flex flex-col justify-center min-w-[120px] max-w-[380px] p-2 rounded-md 
                                 bg-component-faded hover:shadow-xl transition-shadow duration-300 ease-in-out">
