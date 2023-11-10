516c6141fdeff504ef8406a201e2b41266fc7911
piro56 <69094266+piro56@users.noreply.github.com>
2023-11-09 21:27:59 -0500

    more cleanup and gitignore fix

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -51,8 +51,8 @@ export default function Page() {
     }
 
     let nbList = <Spinner className='self-center pt-8 w-[35px] h-[35px]' />;
-    if (error) { 
-        nbList = <h1 className='self-center'>Error Loading Notebooks</h1> 
+    if (error) {
+        nbList = <h1 className='self-center'>Error Loading Notebooks</h1>
     }
     else if (searchData) {
         nbList = <NotebookList notebooks={searchData} />
@@ -65,20 +65,20 @@ export default function Page() {
             <Navigation />
             <div className='self-center flex flex-col w-10/12 lg:w-6/12 h-screen'>
                 <div className='min-h-[20px]'></div>
-                    <div className="flex flex-col gap-10">
+                <div className="flex flex-col gap-10">
 
-                        <div>
-                            <h1 className='text-2xl pl-2 pb-8 font-bold'>My Notebooks</h1>
-                            <div className='flex flex-row grow'>
-                                {searchBar}
-                                <div className='flex flex-row grow justify-end'>
-                                    <button type="button" className="text-white bg-[#309600] font-medium rounded-lg text-sm px-10 py-2">New</button>
-                                </div>
+                    <div>
+                        <h1 className='text-2xl pl-2 pb-8 font-bold'>My Notebooks</h1>
+                        <div className='flex flex-row grow'>
+                            {searchBar}
+                            <div className='flex flex-row grow justify-end'>
+                                <button type="button" className="text-white bg-[#309600] font-medium rounded-lg text-sm px-10 py-2">New</button>
                             </div>
                         </div>
+                    </div>
 
-                        {nbList}
-                        <div className='pb-16'/> {/* for mobile we need some extra space on the bottom to show all cards*/}
+                    {nbList}
+                    <div className='pb-16' /> {/* for mobile we need some extra space on the bottom to show all cards*/}
                 </div>
             </div>
         </div>
