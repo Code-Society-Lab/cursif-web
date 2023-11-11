ddc3b89056debbaabc81ffed5b67d516b0502adb
Chris <rolletchris1@gmail.com>
2023-11-10 18:14:39 -0800

    update format

--- src/components/notebooks/editor.tsx
+++ src/components/notebooks/editor.tsx
@@ -1,14 +1,14 @@
+import EditorToolbar, { modules, formats } from "./editor-toolbar";
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.bubble.css';
 import 'react-quill/dist/quill.snow.css'
 import { useState } from "react";
-import EditorToolbar, { modules, formats } from "./editor-toolbar";
 
 export function BasicEditor({
   page,
   updatePage
 }: {
-  page: any,
+  page: Page,
   updatePage: any
 }) {
   const [value, setValue] = useState(page?.content);
@@ -28,16 +28,16 @@ export function BasicEditor({
       <div className="font-bold text-xl mb-4 ml-8 ">
         <p>{page?.title}</p>
       </div>
-      <div className="relative">
-      <EditorToolbar />
-      <ReactQuill 
-        modules={modules}
-        formats={formats}
-        theme="bubble"
-        value={value}
-        onChange={handleContentChange}
-        placeholder={"Start typing..."}
-      /></div>
+      <div className="relative w-96">
+        <EditorToolbar />
+        <ReactQuill
+          modules={modules}
+          formats={formats}
+          theme="bubble"
+          value={value}
+          onChange={handleContentChange}
+          placeholder={"Start typing..."}
+        /></div>
     </div>
   );
 }
