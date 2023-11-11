3e9157906543116eed83e5795fe01d3ecb340639
Chris <rolletchris1@gmail.com>
2023-11-11 10:26:54 -0800

    added loader, and fix content delay rendering

--- src/components/notebooks/editor.tsx
+++ src/components/notebooks/editor.tsx
@@ -1,8 +1,10 @@
 import EditorToolbar, { modules, formats } from "./editor-toolbar";
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.bubble.css';
-import 'react-quill/dist/quill.snow.css'
-import { useState } from "react";
+import 'react-quill/dist/quill.snow.css';
+import { useState, useEffect } from "react";
+import { PageTitle } from "./edit-title";
+import { Loader } from "@components/loader";
 
 export function BasicEditor({
   page,
@@ -11,7 +13,8 @@ export function BasicEditor({
   page: Page,
   updatePage: any
 }) {
-  const [value, setValue] = useState(page?.content);
+  const [value, setValue] = useState("");
+  const [loading, setLoading] = useState(true);
 
   const handleContentChange = (content: string) => {
     setValue(content);
@@ -23,21 +26,36 @@ export function BasicEditor({
     });
   };
 
+  useEffect(() => {
+    setTimeout(() => {
+      if (page?.content) {
+        setValue(page.content);
+        setLoading(false);
+      }
+    }, 500);
+  }, [page]);
+
   return (
     <div className='mt-6 ml-5'>
-      <div className="font-bold text-xl mb-4 ml-8 ">
-        <p>{page?.title}</p>
-      </div>
+      <PageTitle
+        page={page}
+        updatePage={updatePage}
+      />
       <div className="relative w-96">
         <EditorToolbar />
-        <ReactQuill
-          modules={modules}
-          formats={formats}
-          theme="bubble"
-          value={value}
-          onChange={handleContentChange}
-          placeholder={"Start typing..."}
-        /></div>
+        {loading ? (
+          <Loader />
+        ) : (
+          <ReactQuill
+            modules={modules}
+            formats={formats}
+            theme="bubble"
+            value={value}
+            onChange={handleContentChange}
+            placeholder={"Start typing..."}
+          />
+        )}
+      </div>
     </div>
   );
 }
