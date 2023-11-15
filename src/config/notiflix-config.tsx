1debd86bc18a71a5b872e5963f389d15cc05777c
Chris <rolletchris1@gmail.com>
2023-11-14 20:15:27 -0800

    update Notify config

--- src/config/notiflix-config.tsx
+++ src/config/notiflix-config.tsx
@@ -1,46 +1,52 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { useEffect } from 'react';
 
-Notify.init({
-  position: 'right-top',
-	cssAnimation: true,
-	cssAnimationDuration: 400,
-	cssAnimationStyle: 'zoom',
-	success: {
-		background: '#32c682',
-		textColor: '#fff',
-		childClassName: 'notiflix-notify-success',
-		notiflixIconColor: 'rgba(0,0,0,0.2)',
-		fontAwesomeClassName: 'fas fa-check-circle',
-		fontAwesomeIconColor: 'rgba(0,0,0,0.2)',
-		backOverlayColor: 'rgba(50,198,130,0.2)',
-	},
-	failure: {
-		background: '#ff5549',
-		textColor: '#fff',
-		childClassName: 'notiflix-notify-failure',
-		notiflixIconColor: 'rgba(0,0,0,0.2)',
-		fontAwesomeClassName: 'fas fa-times-circle',
-		fontAwesomeIconColor: 'rgba(0,0,0,0.2)',
-		backOverlayColor: 'rgba(255,85,73,0.2)',
-	},
-	warning: {
-		background: '#eebf31',
-		textColor: '#fff',
-		childClassName: 'notiflix-notify-warning',
-		notiflixIconColor: 'rgba(0,0,0,0.2)',
-		fontAwesomeClassName: 'fas fa-exclamation-circle',
-		fontAwesomeIconColor: 'rgba(0,0,0,0.2)',
-		backOverlayColor: 'rgba(238,191,49,0.2)',
-	},
-	info: {
-		background: '#26c0d3',
-		textColor: '#fff',
-		childClassName: 'notiflix-notify-info',
-		notiflixIconColor: 'rgba(0,0,0,0.2)',
-		fontAwesomeClassName: 'fas fa-info-circle',
-		fontAwesomeIconColor: 'rgba(0,0,0,0.2)',
-		backOverlayColor: 'rgba(38,192,211,0.2)',
-	},
-});
+export function useNotify() {
+	useEffect(() => {
+		Notify.init({
+			position: 'right-top',
+			cssAnimation: true,
+			cssAnimationDuration: 400,
+			cssAnimationStyle: 'zoom',
+			success: {
+				background: '#32c682',
+				textColor: '#fff',
+				childClassName: 'notiflix-notify-success',
+				notiflixIconColor: 'rgba(0,0,0,0.2)',
+				fontAwesomeClassName: 'fas fa-check-circle',
+				fontAwesomeIconColor: 'rgba(0,0,0,0.2)',
+				backOverlayColor: 'rgba(50,198,130,0.2)',
+			},
+			failure: {
+				background: '#ff5549',
+				textColor: '#fff',
+				childClassName: 'notiflix-notify-failure',
+				notiflixIconColor: 'rgba(0,0,0,0.2)',
+				fontAwesomeClassName: 'fas fa-times-circle',
+				fontAwesomeIconColor: 'rgba(0,0,0,0.2)',
+				backOverlayColor: 'rgba(255,85,73,0.2)',
+			},
+			warning: {
+				background: '#eebf31',
+				textColor: '#fff',
+				childClassName: 'notiflix-notify-warning',
+				notiflixIconColor: 'rgba(0,0,0,0.2)',
+				fontAwesomeClassName: 'fas fa-exclamation-circle',
+				fontAwesomeIconColor: 'rgba(0,0,0,0.2)',
+				backOverlayColor: 'rgba(238,191,49,0.2)',
+			},
+			info: {
+				background: '#26c0d3',
+				textColor: '#fff',
+				childClassName: 'notiflix-notify-info',
+				notiflixIconColor: 'rgba(0,0,0,0.2)',
+				fontAwesomeClassName: 'fas fa-info-circle',
+				fontAwesomeIconColor: 'rgba(0,0,0,0.2)',
+				backOverlayColor: 'rgba(38,192,211,0.2)',
+			},
+		});
+	}
+		, []);
+}
 
-export default Notify;
+export { Notify };
