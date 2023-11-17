e826b91f2eef6aef360ae0be406d3f9105e54d26
Chris <rolletchris1@gmail.com>
2023-11-17 11:33:53 -0800

    removed commented code

--- src/components/notebooks/editor-toolbar.tsx
+++ src/components/notebooks/editor-toolbar.tsx
@@ -141,31 +141,14 @@ export const QuillToolbar = () => (
       <button className="ql-strike" />
     </span>
     <span className="ql-formats">
-      {/* <button className="ql-list" value="ordered" /> */}
       <button className="ql-list" value="bullet" />
-      {/* <button className="ql-indent" value="-1" />
-      <button className="ql-indent" value="+1" /> */}
+
     </span>
-    {/* <span className="ql-formats">
-      <button className="ql-script" value="super" />
-      <button className="ql-script" value="sub" />
-      <button className="ql-blockquote" />
-      <button className="ql-direction" />
-    </span>
-    <span className="ql-formats">
-      <select className="ql-align" />
-      <select className="ql-color" />
-      <select className="ql-background" />
-    </span> */}
     <span className="ql-formats">
       <button className="ql-link" />
-      {/* <button className="ql-image" />
-      <button className="ql-video" /> */}
     </span>
     <span className="ql-formats">
-      {/* <button className="ql-formula" /> */}
       <button className="ql-code-block" />
-      {/* <button className="ql-clean" /> */}
     </span>
     <span className="ql-formats">
       <button className="ql-undo">
