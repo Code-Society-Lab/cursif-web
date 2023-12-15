cbbae0732c1bb74c907d0ff32b36c97a9d3402e0
Chris <rolletchris1@gmail.com>
2023-12-15 12:14:30 -0800

    when no page selected, redirect to the first one

--- src/app/(protected)/notebooks/[notebook_id]/page.tsx
+++ src/app/(protected)/notebooks/[notebook_id]/page.tsx
@@ -3,6 +3,7 @@
 import { Sidebar } from "@/components/notebooks/sidebar";
 import { Loader } from "@/components/loader";
 import { useQuery, gql } from "@apollo/client";
+import { useRouter } from 'next/navigation'
 
 const NOTEBOOK_QUERY = gql`
   query GetNotebook($id: ID!) {
@@ -22,14 +23,23 @@ export default function Page({
 }: {
   params: { notebook_id: string; page_id: string };
 }) {
+  const router = useRouter();
   const { data, loading, error } = useQuery(NOTEBOOK_QUERY, {
     variables: {
       id: params.notebook_id,
     },
   });
 
-  if (loading) 
+  if (loading)
     return <Loader />;
+
+  if (!params.page_id) {
+    const firstPageId = data.notebook.pages[0].id;
+    if (firstPageId) {
+      router.replace(`/notebooks/${params.notebook_id}/${firstPageId}`);
+      return <Loader />;
+    }
+  }
 
   return (
     <div className="flex h-screen items-stretch">
