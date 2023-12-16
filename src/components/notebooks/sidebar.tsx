c90a40113270609d7b1a51023c8433629a772afd
Chris <rolletchris1@gmail.com>
2023-12-15 22:34:56 -0800

    added sidebar icone to add new page

--- src/components/notebooks/sidebar.tsx
+++ src/components/notebooks/sidebar.tsx
@@ -1,13 +1,15 @@
-import { Cog8ToothIcon } from "@heroicons/react/24/solid";
 import { EllipsisVerticalIcon } from "@heroicons/react/24/solid";
 import { useRouter } from "next/navigation";
+import { PlusIcon } from "@heroicons/react/24/solid";
 
 export function Sidebar({
   notebook,
   currentPageId,
+  createPage,
 }: {
   notebook: Notebook;
   currentPageId: string;
+  createPage: () => void;
 }) {
   const router = useRouter();
 
@@ -15,13 +17,11 @@ export function Sidebar({
     <nav className="w-[300px] p-2 whitespace-nowrap">
       <div className="flex justify-between">
         <p className="pt-3 pb-6 font-bold text-xl text-ellipsis overflow-hidden" title={notebook.title}>{notebook.title}</p>
-        <span>
-          <a href="settings" className="text-right"><Cog8ToothIcon className="h-5 w-5" /></a>
-        </span>
+        <PlusIcon className="w-6 h-6 text-gray-400" onClick={() => createPage()} />
       </div>
 
       <ul>
-        {notebook.pages.map((page: Page) => (
+        {notebook?.pages?.map((page: Page) => (
           <li
             key={page.id}
             className={`p-1 my-2 cursor-pointer rounded-md ${page.id == currentPageId && "selected"}`}
