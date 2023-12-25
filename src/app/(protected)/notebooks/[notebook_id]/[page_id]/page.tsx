2a945df079003103960a7820edd1a4f27d5d3d60
Chris <rolletchris1@gmail.com>
2023-12-25 10:12:42 -0800

    Update sidebar name + refactor createPage

--- src/app/(protected)/notebooks/[notebook_id]/[page_id]/page.tsx
+++ src/app/(protected)/notebooks/[notebook_id]/[page_id]/page.tsx
@@ -1,32 +1,24 @@
 "use client";
 
-import { Sidebar } from "@/components/notebooks/sidebar";
+import { PagesNavigation } from "@/components/notebooks/pages-navigation";
 import { Loader } from "@/components/loader";
 import { useQuery, useMutation, gql } from "@apollo/client";
 import Notify from "@/config/notiflix-config";
 import { useEffect } from "react";
-import { useRouter } from "next/navigation";
+import { useRouter } from 'next/navigation';
 
 const NOTEBOOK_QUERY = gql`
   query GetNotebook($id: ID!) {
     notebook(id: $id) {
       id
       title
+      ownerId
+      ownerType
       pages {
         id
         title
-      }
-    }
-  }
-`;
-
-const CREATE_PAGE_MUTATION = gql`
-  mutation CreatePage($title: String!, $parentId: ID!, $parentType: String!) {
-    createPage(title: $title,  parentId: $parentId, parentType: $parentType) {
-        id
-        title
         parentId
-        parentType
+      }
     }
   }
 `;
@@ -40,40 +32,18 @@ export default function Page({
   const { data, loading, error } = useQuery(NOTEBOOK_QUERY, {
     variables: {
       id: params.notebook_id,
-    },
-  });
-
-  const [createPage, { data: createPageData, loading: createPageLoading, error: createPageError }] = useMutation(CREATE_PAGE_MUTATION, {
-    variables: {
-      title: "Untitled Page",
-      parentId: data?.notebook?.id,
-      parentType: "notebook",
-    },
-    onCompleted: () => {
-      Notify.success("Page created!");
-    },
-    onError: (error) => {
-      Notify.failure(`${error.message}!`);
-    },
-  });
-
-  useEffect(() => {
-    if (createPageData && createPageData.createPage) {
-      router.replace(`/notebooks/${params.notebook_id}/${createPageData.createPage.id}`);
     }
-  }, [createPageData]);
+  });
 
   if (loading)
     return <Loader />;
 
   return (
     <div className="flex h-screen items-stretch">
-      <Sidebar
+      <PagesNavigation
         notebook={data.notebook}
         currentPageId={params.page_id}
-        createPage={createPage}
       />
-      <p>Text Editor Goes Here</p>
       <div className="flex-[4]" />
     </div>
   );
--- src/app/(protected)/notebooks/[notebook_id]/page.tsx
+++ src/app/(protected)/notebooks/[notebook_id]/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Sidebar } from "@/components/notebooks/sidebar";
+import { PagesNavigation } from "@/components/notebooks/pages-navigation";
 import { Loader } from "@/components/loader";
 import { useQuery, useMutation, gql } from "@apollo/client";
 import Notify from "@/config/notiflix-config";
@@ -23,21 +23,10 @@ const NOTEBOOK_QUERY = gql`
   }
 `;
 
-const CREATE_PAGE_MUTATION = gql`
-  mutation CreatePage($title: String!, $parentId: ID!, $parentType: String!) {
-    createPage(title: $title,  parentId: $parentId, parentType: $parentType) {
-        id
-        title
-        parentId
-        parentType
-    }
-  }
-`;
-
 export default function Page({
   params,
 }: {
-  params: { notebook_id: Notebook["id"]; page_id: Page["id"] };
+  params: { notebook_id: string; page_id: string };
 }) {
   const router = useRouter();
   const { data, loading, error } = useQuery(NOTEBOOK_QUERY, {
@@ -46,35 +35,14 @@ export default function Page({
     }
   });
 
-  const [createPage, { data: createPageData, loading: createPageLoading, error: createPageError }] = useMutation(CREATE_PAGE_MUTATION, {
-    variables: {
-      title: "Untitled Page",
-      parentId: data?.notebook?.id,
-      parentType: "notebook",
-    },
-    onCompleted: () => {
-      Notify.success("Page created!");
-    },
-    onError: (error) => {
-      Notify.failure(`${error.message}!`);
-    },
-  });
-
-  useEffect(() => {
-    if (createPageData && createPageData.createPage) {
-      router.replace(`/notebooks/${params.notebook_id}/${createPageData.createPage.id}`);
-    }
-  }, [createPageData]);
-
   if (loading)
     return <Loader />;
 
   return (
     <div className="flex h-screen items-stretch">
-      <Sidebar
+      <PagesNavigation
         notebook={data.notebook}
         currentPageId={params.page_id}
-        createPage={createPage}
       />
       <div className="flex-[4]" />
     </div>
