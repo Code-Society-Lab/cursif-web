b40873041f507034ad413c8780e16be9ca647b52
Chris <rolletchris1@gmail.com>
2024-03-04 07:25:14 -0800

    Removed default button from the modal

--- src/components/notebooks/modal.tsx
+++ src/components/notebooks/modal.tsx
@@ -2,63 +2,45 @@ import { useState } from 'react';
 import { XMarkIcon } from '@heroicons/react/24/solid';
 
 export default function Modal({
-  buttonTitle,
+  id,
   title,
-  buttonStyle,
+  onClose,
   children,
 }: {
-  buttonTitle: string;
+  id: string;
   title: string;
-  buttonStyle: string;
+  onClose: () => void;
   children: React.ReactNode;
 }) {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const toggleModal = () => {
-    setIsModalOpen(!isModalOpen);
-  };
-
   return (
-    <>
-      <button
-        type="button"
-        className={buttonStyle}
-        onClick={toggleModal}
-      >
-        {buttonTitle}
-      </button>
-
-      {isModalOpen && (
-        <div
-          id="authentication-modal"
-          tabIndex={-1}
-          aria-hidden="true"
-          className="fixed top-0 right-0 left-0 z-50 flex justify-center items-center h-screen"
-        >
-          <div className="relative p-4 w-full max-w-md max-h-full">
-            {/* Modal content */}
-            <div className="relative card rounded-lg shadow  border border-gray-400">
-              {/* Modal header */}
-              <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t">
-                <h3 className="text-xl font-semibold">
-                  {title}
-                </h3>
-                <button
-                  type="button"
-                  className="text-gray-400 hover:bg-gray-200 hover:text-gray-900 rounded-lg inline-flex justify-center items-center"
-                  data-modal-hide="authentication-modal"
-                  onClick={toggleModal}
-                >
-                  <XMarkIcon className="w-8 h-8" />
-                  <span className="sr-only">Close modal</span>
-                </button>
-              </div>
-              {/* Modal body */}
-              {children}
-            </div>
+    <div
+      id={id}
+      tabIndex={-1}
+      aria-hidden="true"
+      className="fixed top-0 right-0 left-0 z-50 flex justify-center items-center h-screen"
+    >
+      <div className="relative p-4 w-full max-w-md max-h-full">
+        {/* Modal content */}
+        <div className="relative card rounded-lg shadow border border-gray-400">
+          {/* Modal header */}
+          <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t">
+            <h3 className="text-xl font-semibold">
+              {title}
+            </h3>
+            <button
+              type="button"
+              className="text-gray-400 hover:bg-gray-200 hover:text-gray-900 rounded-lg inline-flex justify-center items-center"
+              data-modal-hide={id}
+              onClick={onClose}
+            >
+              <XMarkIcon className="w-8 h-8" />
+              <span className="sr-only">Close modal</span>
+            </button>
           </div>
+          {/* Modal body */}
+          {children}
         </div>
-      )}
-    </>
+      </div>
+    </div>
   );
 }
