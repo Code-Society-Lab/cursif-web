bc980660bd9f9ccb67ae0bd77e8c9ba21cee0c92
Chris <rolletchris1@gmail.com>
2024-03-04 10:26:31 -0800

    added title ellipsis for long text

--- src/components/notebooks/modal.tsx
+++ src/components/notebooks/modal.tsx
@@ -24,7 +24,7 @@ export default function Modal({
         <div className="relative card rounded-lg shadow border border-gray-400">
           {/* Modal header */}
           <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t">
-            <h3 className="text-xl font-semibold">
+            <h3 className="whitespace-nowrap text-xl font-semibold text-ellipsis overflow-hidden">
               {title}
             </h3>
             <button
