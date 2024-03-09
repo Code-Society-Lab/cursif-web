923c115fcc4c072fc0519f5e108298fb7530b703
penguinboi <simon.roy1211@gmail.com>
2024-03-09 05:55:12 -0500

    Refactored modal to make it more generic

---
+++ src/components/modal.tsx
@@ -0,0 +1,54 @@
+import { useState } from 'react';
+import { XMarkIcon } from '@heroicons/react/24/solid';
+
+function _getModal(id: string): HTMLDialogElement | null {
+  return document.getElementById(id);
+}
+
+export function openModal(id: string) {
+  _getModal(id)?.showModal();
+}
+
+export function closeModal(id: string) {
+  _getModal(id)?.close();
+}
+
+export function Modal({
+  id,
+  title,
+  children,
+}: {
+  id: string;
+  title: string;
+  children: React.ReactNode;
+}) {
+  document.addEventListener('click', (e) => {
+    if (e.target.closest('dialog')) {
+      if (e.target.closest('.modal')) return;
+      closeModal(id);
+    }
+  });
+
+  return (
+    <>
+      <dialog id={id}>
+        <div className="fixed left-0 top-0 w-full h-full bg-black bg-opacity-50 z-50 overflow-auto backdrop-blur flex justify-center items-center">
+          <div className="modal relative w-full max-w-md max-h-full">
+            <div className="bg-modal relative rounded-lg">
+              <div className="flex items-center justify-between p-4 md:p-5 rounded-t">
+                <h3 className="whitespace-nowrap font-semibold text-xl text-ellipsis overflow-hidden">
+                  {title}
+                </h3>
+
+                <button className="hover:bg-component rounded-lg inline-flex justify-center items-center" onClick={() => closeModal(id)}>
+                  <XMarkIcon className="w-8 h-8" />
+                </button>
+              </div>
+              {children}
+            </div>
+          </div>
+        </div>
+      </dialog>
+    </>
+  );
+}
