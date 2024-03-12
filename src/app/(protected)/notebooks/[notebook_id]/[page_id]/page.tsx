b4a305c7b96287fef8e17b6bbb816a27cbc09c58
Chris <rolletchris1@gmail.com>
2024-03-12 14:32:23 -0700

    init markdown editor

--- src/app/(protected)/notebooks/[notebook_id]/[page_id]/page.tsx
+++ src/app/(protected)/notebooks/[notebook_id]/[page_id]/page.tsx
@@ -2,7 +2,13 @@
 
 import { Sidebar } from "@/components/notebooks/sidebar";
 import { Loader } from "@/components/loader";
-import { useQuery, gql } from "@apollo/client";
+import { useQuery, useMutation, gql } from "@apollo/client";
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
+import { useState } from "react";
+import { Spinner } from '@components/loader';
+import { useEffect } from "react";
+import Notify from '@config/notiflix-config';
 
 const NOTEBOOK_QUERY = gql`
   query GetNotebook($id: ID!) {
@@ -17,6 +23,26 @@ const NOTEBOOK_QUERY = gql`
   }
 `;
 
+const PAGE_QUERY = gql`
+   query GetPage($id: ID!) {
+     page(id: $id) {
+       id
+       title
+       content
+     }
+   }
+ `;
+
+const UPDATE_PAGE_MUTATION = gql`
+   mutation UpdatePage($id: ID!, $content: String!, $title: String) {
+     updatePage(id: $id, content: $content, title: $title) {
+       id
+       title
+       content
+     }
+   }
+ `;
+
 export default function Page({
   params,
 }: {
@@ -28,7 +54,37 @@ export default function Page({
     },
   });
 
-  if (loading) 
+  const { data: pageData, loading: pageLoading, error: pageError } = useQuery(PAGE_QUERY, {
+    variables: {
+      id: params.page_id,
+    },
+  });
+
+  const [updatePage, { data: pageUpdateData, loading: pageUpdateLoading, error: pageUpdateError }] = useMutation(UPDATE_PAGE_MUTATION, {
+    variables: {
+      id: params.page_id,
+      title: pageData?.page.title,
+    }
+  });
+
+  const [value, setValue] = useState(pageData?.page.content);
+  const onChange = (value: string) => setValue(value);
+
+  const onSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    updatePage({
+      variables: {
+        id: params.page_id,
+        content: value,
+      },
+    });
+    Notify.success('Note saved!');
+  };
+
+  if (loading)
+    return <Loader />;
+
+  if (pageLoading)
     return <Loader />;
 
   return (
@@ -37,7 +93,21 @@ export default function Page({
         notebook={data.notebook}
         currentPageId={params.page_id}
       />
-      <div className="flex-[4]" />
+      <div className="flex-[4]" >
+        <form onSubmit={onSubmit}>
+
+          <div className="relative">
+            <SimpleMDE value={value} onChange={onChange} />
+          </div>
+
+          <button id="save-button" className="button !bg-accent float-right mt-6" type="submit">
+            <span className="spinner"><Spinner /></span>
+            <span className="label">Save</span>
+          </button>
+
+        </form>
+      </div>
+
     </div>
   );
 }
