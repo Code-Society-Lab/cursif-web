b68ea500bfd751a041e4158a425d1e9e08b5a839
Chris <rolletchris1@gmail.com>
2024-03-12 18:03:52 -0700

    refactor page update

--- src/app/(protected)/notebooks/[notebook_id]/[page_id]/page.tsx
+++ src/app/(protected)/notebooks/[notebook_id]/[page_id]/page.tsx
@@ -2,13 +2,8 @@
 
 import { Sidebar } from "@/components/notebooks/sidebar";
 import { Loader } from "@/components/loader";
-import { useQuery, useMutation, gql } from "@apollo/client";
-import SimpleMDE from "react-simplemde-editor";
-import "easymde/dist/easymde.min.css";
-import { useState } from "react";
-import { Spinner } from '@components/loader';
-import { useEffect } from "react";
-import Notify from '@config/notiflix-config';
+import { useQuery, gql } from "@apollo/client";
+import PageEditor from "@/components/pages/editor";
 
 const NOTEBOOK_QUERY = gql`
   query GetNotebook($id: ID!) {
@@ -23,68 +18,19 @@ const NOTEBOOK_QUERY = gql`
   }
 `;
 
-const PAGE_QUERY = gql`
-   query GetPage($id: ID!) {
-     page(id: $id) {
-       id
-       title
-       content
-     }
-   }
- `;
-
-const UPDATE_PAGE_MUTATION = gql`
-   mutation UpdatePage($id: ID!, $content: String!, $title: String) {
-     updatePage(id: $id, content: $content, title: $title) {
-       id
-       title
-       content
-     }
-   }
- `;
-
 export default function Page({
   params,
 }: {
   params: { notebook_id: string; page_id: string };
 }) {
+
   const { data, loading, error } = useQuery(NOTEBOOK_QUERY, {
     variables: {
       id: params.notebook_id,
     },
   });
 
-  const { data: pageData, loading: pageLoading, error: pageError } = useQuery(PAGE_QUERY, {
-    variables: {
-      id: params.page_id,
-    },
-  });
-
-  const [updatePage, { data: pageUpdateData, loading: pageUpdateLoading, error: pageUpdateError }] = useMutation(UPDATE_PAGE_MUTATION, {
-    variables: {
-      id: params.page_id,
-      title: pageData?.page.title,
-    }
-  });
-
-  const [value, setValue] = useState(pageData?.page.content);
-  const onChange = (value: string) => setValue(value);
-
-  const onSubmit = (event: React.FormEvent) => {
-    event.preventDefault();
-    updatePage({
-      variables: {
-        id: params.page_id,
-        content: value,
-      },
-    });
-    Notify.success('Note saved!');
-  };
-
   if (loading)
-    return <Loader />;
-
-  if (pageLoading)
     return <Loader />;
 
   return (
@@ -94,18 +40,7 @@ export default function Page({
         currentPageId={params.page_id}
       />
       <div className="flex-[4]" >
-        <form onSubmit={onSubmit}>
-
-          <div className="relative">
-            <SimpleMDE value={value} onChange={onChange} />
-          </div>
-
-          <button id="save-button" className="button !bg-accent float-right mt-6" type="submit">
-            <span className="spinner"><Spinner /></span>
-            <span className="label">Save</span>
-          </button>
-
-        </form>
+        <PageEditor page_id={params.page_id} />
       </div>
 
     </div>
