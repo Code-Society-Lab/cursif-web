a9c5faae23f08be9e0f5358971ff97266cc02433
Chris <rolletchris1@gmail.com>
2024-03-14 09:57:12 -0700

    redirect to the first page when a notebook is selected.

--- src/app/(protected)/notebooks/[notebook_id]/page.tsx
+++ src/app/(protected)/notebooks/[notebook_id]/page.tsx
@@ -3,6 +3,7 @@
 import { Sidebar } from "@/components/notebooks/sidebar";
 import { Loader } from "@/components/loader";
 import { useQuery, gql } from "@apollo/client";
+import { useRouter } from 'next/navigation';
 
 const NOTEBOOK_QUERY = gql`
   query GetNotebook($id: ID!) {
@@ -20,20 +21,34 @@ const NOTEBOOK_QUERY = gql`
 export default function Page({
   params,
 }: {
-  params: { notebook_id: Notebook["id"]; page_id: Page["id"] };
+  params: { notebook_id: string; page_id: string };
 }) {
+  const router = useRouter();
   const { data, loading, error } = useQuery(NOTEBOOK_QUERY, {
     variables: {
       id: params.notebook_id,
     },
   });
 
-  if (loading) 
+  if (loading)
     return <Loader />;
+
+  // TO-DO: Remove this once we have a proper pages dashboard
+  // For now, redirect to the first page if no page is selected.
+  if (!params.page_id) {
+    const firstPageId = data.notebook.pages[0].id;
+    if (firstPageId) {
+      router.replace(`/notebooks/${params.notebook_id}/${firstPageId}`);
+      return <Loader />;
+    }
+  }
 
   return (
     <div className="flex h-screen items-stretch">
-      <Sidebar notebook={data.notebook} />
+      <Sidebar
+        notebook={data.notebook}
+        currentPageId={params.page_id}
+      />
       <div className="flex-[4]" />
     </div>
   );
