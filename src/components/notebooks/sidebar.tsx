88a43547bd81a2739e07d9498ecfd6b0ebb1b215
Chris <rolletchris1@gmail.com>
2024-03-14 21:38:09 -0700

    added descriptive alt title to Cogs Icons

--- src/components/notebooks/sidebar.tsx
+++ src/components/notebooks/sidebar.tsx
@@ -18,8 +18,8 @@ export function Sidebar({
     <nav className="w-[300px] p-2">
       <div className="flex justify-between">
         <p className="pt-3 pb-6 font-bold text-xl text-ellipsis overflow-hidden" title={notebook.title}>{notebook.title}</p>
-        <span className='cursor-pointer'>
-          <Cog8ToothIcon className="h-5 w-5" onClick={() => openModal('edit-notebook-modal')} />
+        <span className='cursor-pointer' title="Update Notebook">
+          <Cog8ToothIcon className="h-5 w-5" onClick={() => openModal('update-notebook-modal')}/>
         </span>
       </div>
 
@@ -32,13 +32,13 @@ export function Sidebar({
           >
             <div className="flex justify-between items-center">
               <p className="text-ellipsis overflow-hidden" title={page.title}>{page.title}</p>
-              <a href="#" className="text-right"><EllipsisVerticalIcon className="h-5 w-5" /></a>
+              <a href="#" className="text-right" title="Update Page"><EllipsisVerticalIcon className="h-5 w-5" /></a>
             </div>
           </li>
         ))}
       </ul>
 
-      <Modal id='edit-notebook-modal' title='Update Notebook'>
+      <Modal id='update-notebook-modal' title='Update Notebook'>
         <NotebookForm notebook={notebook} />
       </Modal>
     </nav>
