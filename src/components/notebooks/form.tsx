9d3cfaf3080d625d2a73358ffde3f457fe04ba61
Chris <rolletchris1@gmail.com>
2024-05-15 16:18:32 -0700

    refactor notebook form component and add danger zone prevention

--- src/components/notebooks/form.tsx
+++ src/components/notebooks/form.tsx
@@ -1,10 +1,12 @@
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
-import { useAuth } from '@/components/auth-provider';
-import { useMutation, gql } from '@apollo/client';
-
-import { Notify } from '@config/notiflix-config';
 import { Spinner } from '@/components/loader';
+import { Notify } from '@config/notiflix-config';
+import { useMutation, gql } from '@apollo/client';
+import { useAuth } from '@/components/auth-provider';
+import { Modal, openModal, closeModal } from '@/components/modal';
+import { ExclamationTriangleIcon } from '@heroicons/react/24/solid';
+import DeleteNotebookForm from '@/components/notebooks/delete';
 
 const CREATE_NOTEBOOK_MUTATION = gql`
   mutation CreateNotebook($title: String!, $description: String, $ownerId: ID!) {
@@ -70,7 +72,7 @@ export default function NotebookForm({ notebook, onComplete }: { notebook?: Note
 
   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    
+
     if (notebook) {
       updateNotebook({ variables: { title, description } });
     } else {
@@ -87,30 +89,56 @@ export default function NotebookForm({ notebook, onComplete }: { notebook?: Note
 
   return (
     <div className="p-4 md:p-5">
-      <form onSubmit={onSubmit} className="space-y-4">
-        <input
-          className="input w-full"
-          type="text"
-          placeholder="Title"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-          required={true}
-        />
-        <textarea
-          rows={4}
-          maxLength={200}
-          className="input w-full resize-none"
-          placeholder="Write your notebook description here... (Optional)"
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-          required={false}
-        />
-        <div className="flex justify-end">
-          <button type="submit" className="button bg-new">
-            <span className="label">{notebook ? 'Save' : 'Create'}</span>
-          </button>
-        </div>
-      </form>
+      <span className='text-sm font-bold'>
+        {notebook ? 'Update Notebook' : ''}
+      </span>
+      <div className="mt-1">
+        <form onSubmit={onSubmit} className="space-y-4">
+          <input
+            className="input w-full"
+            type="text"
+            placeholder="Title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            required={true}
+          />
+          <textarea
+            rows={4}
+            maxLength={200}
+            className="input w-full resize-none"
+            placeholder="Write your notebook description here... (Optional)"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            required={false}
+          />
+          <div className="flex justify-end">
+            <button type="submit" className="button bg-new">
+              <span className="label">{notebook ? 'Save' : 'Create'}</span>
+            </button>
+          </div>
+        </form>
+      </div>
+      {notebook ?
+        <>
+          <hr className="my-4" />
+          <span className='flex text-sm font-bold'>
+            <ExclamationTriangleIcon className="h-5 w-5 text-red-500 mr-2" /> Danger Zone
+          </span>
+          <div className="border border-red-600 rounded-4 p-2 mt-1 text-xs">
+            <b>Delete this notebook.</b>
+            <br />
+            This action is irreversible. All pages in this notebook will be deleted. Make sure you have a backup of your data.
+            <span className='flex justify-end'>
+              <button className='button bg-delete p-2 pt-1 pb-1' onClick={() => openModal('delete-notebook-modal')}>
+                Delete
+              </button>
+            </span>
+          </div>
+          <Modal id='delete-notebook-modal' title='Delete Notebook'>
+            <DeleteNotebookForm notebook={notebook} onComplete={() => { closeModal('delete-notebook-modal'); }} onUpdate={() => router.push(`/notebooks`)} />
+          </Modal>
+        </> : ''
+      }
     </div>
   );
 }
