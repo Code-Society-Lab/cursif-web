e920648ae2a09358e502deb894c8cd77a8e16444
penguinboi <simon.roy1211@gmail.com>
2024-05-17 01:11:09 -0400

    Switch to cookie authentication and added middleware to handle / routing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,8 @@
 "use client"
 
-import { useRouter } from 'next/navigation';
-import { usePathname } from 'next/navigation';
 import { Loader } from '@/components/loader';
 
 export default function Page() {
-  const router   = useRouter();
-  const pathname = usePathname();
-
-  if (pathname === '/') {
-    router.replace('/login');
-  }
-
   return (
     <Loader />
   );
