53873b5dbfeca4c3cd9b9c8f995de584306c3d7f
Chris <rolletchris1@gmail.com>
2024-07-12 20:06:01 -0700

    chore: Update settings dropdown component styling

--- src/components/settings.tsx
+++ src/components/settings.tsx
@@ -13,7 +13,7 @@ export default function SettingsDropdown({ user }: { user: { username: string; e
 
   return (
     <div className="relative inline-block">
-      <button onClick={toggleDropdown} className="relative z-10 flex items-center p-2 text-sm bg-component rounded-md">
+      <button onClick={toggleDropdown} className="relative z-10 flex items-center p-2 text-sm bg-component-faded rounded-md">
         <span className="mx-1">Settings</span>
         <svg className="w-5 h-5 mx-1" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M12 15.713L18.01 9.70299L16.597 8.28799L12 12.888L7.40399 8.28799L5.98999 9.70199L12 15.713Z" fill="currentColor"></path>
@@ -43,10 +43,10 @@ export default function SettingsDropdown({ user }: { user: { username: string; e
 
         <hr className="border-gray-200" />
 
-        <a href="#" className="block px-4 py-3 text-sm transition-colors duration-300 transform hover:bg-gray-400">
+        <a href="#" className="block px-4 py-3 text-sm transition-colors duration-300 transform hover:bg-component-faded">
           Settings
         </a>
-        <a href="/logout" className="block px-4 py-3 text-sm transition-colors duration-300 transform hover:bg-gray-400">
+        <a href="/logout" className="block px-4 py-3 text-sm transition-colors duration-300 transform hover:bg-component-faded">
           Log Out
         </a>
       </div>
