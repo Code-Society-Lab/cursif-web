2550c98444e87e6e8363eb6f8a38165bfdb3f1d2
Chris <rolletchris1@gmail.com>
2024-07-13 21:50:01 -0700

    refactor: Update Hide pages navigation menu by default on mobile devices + Desktop

--- src/app/(protected)/notebooks/[notebook_id]/[page_id]/page.tsx
+++ src/app/(protected)/notebooks/[notebook_id]/[page_id]/page.tsx
@@ -1,9 +1,10 @@
 "use client";
 
 import { PagesNavigation } from "@/components/notebooks/pages-navigation";
-import { useQuery, gql } from "@apollo/client";
+import { useState, useEffect } from 'react';
 import { Loader } from "@/components/loader";
-import { useState } from "react";
+import { useQuery, gql } from "@apollo/client";
+import { Bars3Icon } from '@heroicons/react/20/solid';
 
 import PageEditor from "@/components/pages/editor";
 
@@ -31,8 +32,20 @@ export default function Page({
   const { data, loading, error, refetch } = useQuery(NOTEBOOK_QUERY, {
     variables: {
       id: params.notebook_id,
-    },
+    }
   });
+
+  // On mobile, hid the pages navigation menu by default
+  useEffect(() => {
+    const handleResize = () => {
+      setIsNavVisible(window.innerWidth > 768);
+    };
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   if (error)
     return <div>Error loading notebook: {error.message}</div>;
@@ -49,10 +62,15 @@ export default function Page({
           onUpdate={() => refetch()}
         />
       )}
-      <div
-        className={`resizable-divider ${isNavVisible ? '' : 'closed'}`}
-        onClick={() => setIsNavVisible(!isNavVisible)}
-      />
+      <div className="relative">
+        <div
+          className="p-1 mt-2 rounded cursor-pointer"
+          onClick={() => setIsNavVisible(!isNavVisible)}
+          title='Toggle pages navigation bar'
+        >
+          <Bars3Icon className="h-6 w-6" />
+        </div>
+      </div>
       <div className="flex-[4]">
         <PageEditor page_id={params.page_id} />
       </div>
