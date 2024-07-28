ce1dd48bcc40727b58ba86f5e076dd522a879330
penguinboi <simon.roy1211@gmail.com>
2024-07-28 19:13:20 -0400

    Force dynamic on notebooks

--- src/app/(protected)/notebooks/layout.tsx
+++ src/app/(protected)/notebooks/layout.tsx
@@ -7,6 +7,8 @@ export const metadata: Metadata = {
   description: 'My notebooks!',
 }
 
+export const dynamic = 'force-dynamic';
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return <>{children}</>;
 }
